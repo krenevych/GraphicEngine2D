48c85e87511154b2289795e8403b3ccfea2d52b4
Andrii Krenevych <krenevych@knu.ua>
2025-01-24 12:01:39 +0200

    документація

--- src/main.py
+++ src/main.py
@@ -4,6 +4,9 @@ from src.math.Vec3 import Vec3
 
 
 def scene():
+    """
+    Головна функція, у якій опишіть всі обʼєкти, що мають відображатися на сцені
+    """
     p1 = Vec3(0, 0, 1)
     p2 = Vec3(1, 0, 1)
     p3 = Vec3(0, 1, 1)
@@ -22,11 +25,11 @@ def scene():
 
 if __name__ == '__main__':
     draw_scene(
-        scene=scene,
-        coordinate_rect=(-1, -1, 2, 2),
-        # grid_show=False,
-        base_axis_show=False,
-        axis_show=True,
-        axis_color="red",
-        axis_line_style="-."
+        scene=scene,       # функція у якій описується сцена
+        coordinate_rect=(-1, -1, 2, 2),  #  розмірність системи координат
+        grid_show=False,   #  чи показувати координатну сітку
+        base_axis_show=False,  # чи показувати базові осі зображення
+        axis_show=True,    # чи показувати осі координат
+        axis_color="red",  # колір осей координат
+        axis_line_style="-."  #  стиль ліній осей координат
     )

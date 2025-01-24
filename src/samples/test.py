572fbf3b80c3b4a6ab7001d88b60b84433b221e6
Andrii Krenevych <krenevych@knu.ua>
2025-01-24 12:36:06 +0200

    документація - зображення

--- src/samples/test.py
+++ src/samples/test.py
@@ -19,20 +19,20 @@ def scene():
         ('P3', (-0.1, 0.1)),
     ]
 
-    line2d(P1, P2, P3, closed=True, color="grey", labels=labels, line_style="--", labels_fontsize=16)
+    line2d(P1, P2, P3, closed=True, color="blue", labels=labels, line_style="--", labels_fontsize=16)
 
-    T1 = Mat3x3.translation(0, -1, 1)
+    # T1 = Mat3x3.translation(0, -1, 1)
 
     R = Mat3x3.rotationZ(30, False)
     # T = Mat3x3.translation(2, 2, 1)
-    T = Mat3x3.translation(0, -1, 1)
+    T = Mat3x3.translation(2, 1, 1)
     S = Mat3x3.scale(2, 2)
 
-    # transform = T1.inverse() @ T @ R @ S @ T1
+    transform = T @ R @ S
     # transform = T.inverse() @ S @ T1
     # transform =  T1 @ S
     # transform = T1.inverse() @ R @ T1
-    transform = R
+    # transform = R
     # transform =  S @ T1
 
     # trans_inv = transform.inv
@@ -47,23 +47,24 @@ def scene():
     line2d(P1_, P2_, P3_, closed=True, color="red", labels=labels, line_style="solid")
 
     T3 = Mat3x3.translation(0, -1, 1)
-    transform = T1.inverse() @ R @ T3
+    # transform = T1.inverse() @ R @ T3
     P1_ = transform @ P1
     P2_ = transform @ P2
     P3_ = transform @ P3
 
-    line2d(P1_, P2_, P3_, closed=True, color="blue", labels=labels, line_style="solid")
+    # line2d(P1_, P2_, P3_, closed=True, color="blue", labels=labels, line_style="solid")
 
 
 if __name__ == '__main__':
     draw_scene(
         scene=scene,
-        coordinate_rect=(-1, -1, 2, 2),
+        title="Triangle transformation",
+        coordinate_rect=(-2, -2, 4, 4),
         # grid_show=False,
         base_axis_show=False,
-        # axis_show=True,
-        # axis_color="red",
-        # axis_line_style="-."
+        axis_show=True,
+        axis_color="grey",
+        axis_line_style="-."
     )
 
 

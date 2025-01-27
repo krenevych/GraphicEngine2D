6a8577e3ab2e1b4eb146235f075c32eae0a3d9dd
Andrii Krenevych <krenevych@knu.ua>
2025-01-27 19:07:28 +0200

    Simple figures

---
+++ src/engine/simple/SimplePoint.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+from src.base.points import draw_point
+from src.base.scene import draw_scene
+from src.math.Mat3x3 import Mat3x3
+from src.math.Vec3 import vertex
+
+
+class SimplePoint:
+
+    def __init__(self, x=0, y=0):
+        self.vertex = vertex(x, y)
+        self.transformation = Mat3x3()
+        self.color = "black"
+
+    def draw(self):
+        ps =  self.transformation * self.vertex
+        draw_point(ps.xy, color=self.color)
+
+
+def scene():
+    point = SimplePoint(1, 1 )
+
+    point.color = "blue"  # колір ліній
+    point.draw()
+
+    R = Mat3x3.rotation(np.radians(45))
+    S = Mat3x3.scale(2, 3)
+    T = Mat3x3.translation(1, 1)
+
+    point.color = "red"  # колір ліній
+    point.transformation = T * R * S
+    point.draw()
+
+
+if __name__ == '__main__':
+    draw_scene(
+        scene=scene,  # функція у якій описується сцена
+        image_size=(5, 5),  # розмір зображення: 1 - 100 пікселів
+        coordinate_rect=(-2, -2, 6, 6),  # розмірність системи координат
+        title="Picture",  # заголовок рисунка
+        grid_show=False,  # чи показувати координатну сітку
+        base_axis_show=False,  # чи показувати базові осі зображення
+        axis_show=True,  # чи показувати осі координат
+        axis_color=("red", "green"),  # колір осей координат
+        axis_line_style="-."  # стиль ліній осей координат
+    )
---
+++ src/engine/simple/SimplePolygon.py
@@ -0,0 +1,63 @@
+import numpy as np
+
+from src.base.broken_line import draw_broken_line
+from src.base.scene import draw_scene
+from src.math.Mat3x3 import Mat3x3
+from src.math.Vec3 import vertex
+
+
+class SimplePolygon:
+
+    def __init__(self, *vertices):
+        assert all(isinstance(item, (float, int)) for item in vertices) and len(vertices) % 2 == 0
+
+        self.__geometry = [vertex(vertices[i], vertices[i + 1]) for i in range(0, len(vertices), 2)]
+        self.transformation = Mat3x3()
+        self.color = "black"
+        self.line_style = "-"
+
+    @property
+    def transformed_geometry(self):
+        return [self.transformation * point for point in self.__geometry]
+
+    def draw(self):
+        transformed_geometry = self.transformed_geometry
+        ps = [el.xy for el in transformed_geometry]
+        ps.append(transformed_geometry[0].xy)  # closed line
+
+        draw_broken_line(ps, color=self.color, line_style=self.line_style)
+
+
+def scene():
+    rect = SimplePolygon(0, 0,
+                         1, 0,
+                         1, 1,
+                         0, 1,
+                         )
+
+    rect.color = "blue"  # колір ліній
+    rect.line_style = "--"  # стиль ліній
+    rect.draw()
+
+    R = Mat3x3.rotation(np.radians(45))
+    S = Mat3x3.scale(2, 3)
+    T = Mat3x3.translation(1, 1)
+
+    rect.color = "red"  # колір ліній
+    rect.line_style = "-"  # стиль ліній
+    rect.transformation = T * R * S
+    rect.draw()
+
+
+if __name__ == '__main__':
+    draw_scene(
+        scene=scene,  # функція у якій описується сцена
+        image_size=(5, 5),  # розмір зображення: 1 - 100 пікселів
+        coordinate_rect=(-2, -2, 6, 6),  # розмірність системи координат
+        title="Picture",  # заголовок рисунка
+        grid_show=False,  # чи показувати координатну сітку
+        base_axis_show=False,  # чи показувати базові осі зображення
+        axis_show=True,  # чи показувати осі координат
+        axis_color=("red", "green"),  # колір осей координат
+        axis_line_style="-."  # стиль ліній осей координат
+    )

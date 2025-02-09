471e6ffe55a385019a309f0339466f489f8a8110
Andrii Krenevych <krenevych@knu.ua>
2025-02-09 18:56:06 +0200

    Scene 3D

--- src/engine/scene/Scene.py
+++ src/engine/scene/Scene.py
@@ -12,24 +12,29 @@ class Scene(ABC):
                  image_size=(5, 5),
                  coordinate_rect=(-1, -1, 1, 1),
                  title="Picture",
-                 base_axis_show=True,
-                 axis_show=False, axis_color=("red", "green"), axis_line_style="-.", axis_line_width = 1.0,
-                 grid_show=True, grid_line_linestyle="solid", greed_alpha=1.0,
-                 keep_aspect_ratio=True, ):
+                 base_axis_show=False,
+                 grid_show=True,
+                 axis_show=True,
+                 axis_show_from_origin=True,
+                 axis_color=("red", "green", "blue"),
+                 axis_line_style="-.",
+                 axis_line_width=1.0,
+
+                 ):
         self.image_size = image_size
         self.coordinate_rect = coordinate_rect
         self.title = title
         self.base_axis_show = base_axis_show
         self.axis_show = axis_show
+        self.axis_show_from_origin = axis_show_from_origin
         self.axis_color = axis_color
         self.axis_line_style = axis_line_style
         self.axis_line_width = axis_line_width
         self.grid_show = grid_show
-        self.grid_line_linestyle = grid_line_linestyle
-        self.greed_alpha = greed_alpha
-        self.keep_aspect_ratio = keep_aspect_ratio
 
         self.figure = plt.figure(figsize=self.image_size)
+        self.plt_axis = self.figure.add_subplot(111, projection="3d")
+
         self.figures = {}
 
         self.frame_sequence: list[Frame] = []
@@ -51,29 +56,42 @@ class Scene(ABC):
 
     def show_axes(self):
         if self.axis_show:
-            draw_axes(self.coordinate_rect, self.axis_color, self.axis_line_style, self.axis_line_width)
+            draw_axes(
+                self.plt_axis,
+                self.coordinate_rect,
+                self.axis_color,
+                self.axis_line_style,
+                linewidth=self.axis_line_width,
+                axis_show_from_origin=self.axis_show_from_origin,
+            )
 
-        plt.xlim(self.coordinate_rect[0], self.coordinate_rect[2])
-        plt.ylim(self.coordinate_rect[1], self.coordinate_rect[3])
+        self.plt_axis.set_xlim([self.coordinate_rect[0], self.coordinate_rect[3]])
+        self.plt_axis.set_ylim([self.coordinate_rect[1], self.coordinate_rect[4]])
+        self.plt_axis.set_zlim([self.coordinate_rect[2], self.coordinate_rect[5]])
 
     def setup_base_parameters(self):
-        # Відключення стандартних осей
+
+        # self.plt_axis.view_init(elev=110,
+        #                         azim=205,
+        #                         roll=-65)
+
+        self.plt_axis.view_init(elev=110,
+                                azim=225,
+                                roll=-45)
+
+        # self.plt_axis.view_init(elev=110,
+        #                         azim=245,
+        #                         roll=-25)
+
+        # # Відключення стандартних осей
         if not self.base_axis_show:
-            plt.gca().spines['bottom'].set_visible(False)
-            plt.gca().spines['left'].set_visible(False)
-            plt.gca().spines['top'].set_visible(False)
-            plt.gca().spines['right'].set_visible(False)
+            self.plt_axis.axis('off')
 
-        if self.keep_aspect_ratio:
-            plt.gca().set_aspect('equal', adjustable='box')
-
-        if self.grid_show:
-            plt.grid(self.grid_show, linestyle=self.grid_line_linestyle, alpha=self.greed_alpha, )
-        else:
-            plt.grid(False)
+        self.plt_axis.grid(self.grid_show)
 
     def set_title(self):
-        plt.title(self.title)
+        # Назва графіка
+        self.plt_axis.set_title(self.title)
 
     def draw(self, name=None):
         if name is None:

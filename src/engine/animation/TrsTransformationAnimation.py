fixed decomposition function and quaternion rotation animation

src/engine/animation/TrsTransformationAnimation.py
@@ -5,19 +5,16 @@ from src.math.utils_matrix import decompose_affine
 
 class TrsTransformationAnimation(Animation):
 
-    # def __init__(self, end, **kwargs):
-    #     super().__init__(end, **kwargs)
-    #
-    #     end_translation, end_scale, end_rotation, end_axis, end_angle = Mat4x4.decompose_affine(self.end)
-    #     print(end_axis, np.degrees(end_angle))
 
     def current_transformation(self, frame):
         start_translation, start_rotation, start_scale, start_axis, start_angle = decompose_affine(self.start)
         end_translation, end_rotation, end_scale, end_axis, end_angle = decompose_affine(self.end_rotation)
 
-        translation = start_translation + (end_translation - start_translation) * (frame / self.frames)
-        angle = start_angle + (end_angle - start_angle) * (frame / self.frames)
-        scales = start_scale + (end_scale - start_scale) * (frame / self.frames)
+        t = frame / self.frames
+
+        translation = start_translation + (end_translation - start_translation) * t
+        angle = start_angle + (end_angle - start_angle) * t
+        scales = start_scale + (end_scale - start_scale) * t
 
         T = Mat4x4.translation(translation)
         R = Mat4x4.rotation(angle, end_axis)

src/samples/lab07/quaternion_rotation.py
@@ -1,6 +1,8 @@
 import numpy as np
 
+from src.engine.animation.QuaternionAnimation import QuaternionAnimation
 from src.engine.animation.RotationAnimation import RotationAnimation
+from src.engine.animation.TrsTransformationAnimation import TrsTransformationAnimation
 from src.engine.model.SimplePolygon import SimplePolygon
 from src.engine.scene.AnimatedScene import AnimatedScene
 from src.math.Mat4x4 import Mat4x4
@@ -37,7 +39,7 @@ if __name__ == '__main__':
     interval = 5
 
     animation_x = RotationAnimation(  # Rx
-        end=np.radians(angle_x),
+        end=angle_x,
         axis=OX,
         frames=frames_num,
         interval=interval,
@@ -48,10 +50,10 @@ if __name__ == '__main__':
     OY1 = Rx * OY
     OZ1 = Rx * OZ
 
-    Ry1 = Mat4x4.rotation(angle_y, OY1, False)
+    Ry1 = Mat4x4.rotation(angle_y, OY1)
 
     animation_y = RotationAnimation(  # Ry1
-        end=np.radians(angle_y),
+        end=angle_y,
         axis=OY1,
         frames=frames_num,
         interval=interval,
@@ -59,12 +61,12 @@ if __name__ == '__main__':
         apply_geometry_transformation_on_finish=True,
     )
 
-    OZ2 = Mat4x4.rotation(angle_y, OY1, False) * OZ1
+    OZ2 = Mat4x4.rotation(angle_y, OY1) * OZ1
 
-    Rz2 = Mat4x4.rotation(angle_z, OZ2, False)
+    Rz2 = Mat4x4.rotation(angle_z, OZ2)
 
     animation_z = RotationAnimation(
-        end=np.radians(angle_z),
+        end=angle_z,
         axis=OZ2,
         frames=frames_num,
         interval=interval,
@@ -72,6 +74,21 @@ if __name__ == '__main__':
         apply_geometry_transformation_on_finish=True,
     )
 
+    animation = TrsTransformationAnimation(
+        end=R_final,
+        frames=frames_num,
+        interval=5,
+        channel=RECT_KEY,
+        apply_geometry_transformation_on_finish=True,
+    )
+
+    animation_quat = QuaternionAnimation(
+        end=q_final,
+        frames=frames_num,
+        interval=5,
+        channel=RECT_KEY,
+        apply_geometry_transformation_on_finish=True,
+    )
 
     class SimplePolygonScene(AnimatedScene):
 
@@ -110,9 +127,11 @@ if __name__ == '__main__':
         axis_line_style="-."  # стиль ліній осей координат
     ).prepare()
 
-    animated_scene.add_animation(animation_x)
-    animated_scene.add_animation(animation_y)
-    animated_scene.add_animation(animation_z)
+    # animated_scene.add_animation(animation_x)
+    # animated_scene.add_animation(animation_y)
+    # animated_scene.add_animation(animation_z)
 
+    # animated_scene.add_animation(animation)
+    animated_scene.add_animation(animation_quat)
 
     animated_scene.animate()

src/samples/quat/rotation.py
@@ -3,7 +3,7 @@ import numpy as np
 from src.math.Mat4x4 import Mat4x4
 from src.math.Quaternion import Quaternion
 from src.math.Vec3 import Vec3
-from src.math.utils_matrix import decompose_translation_scale_quaternion
+from src.math.utils_matrix import decompose_translation_quaternion_scale
 
 if __name__ == '__main__':
     angle_x = np.radians(30)
@@ -22,7 +22,7 @@ if __name__ == '__main__':
     q = qz * qy * qx
     Rq = q.toRotationMatrix()
 
-    T, S, quat_from_R = decompose_translation_scale_quaternion(R)
+    T, S, quat_from_R = decompose_translation_quaternion_scale(R)
     print(f"{q=}")
     print(f"{quat_from_R=}")
     print(f"{quat_from_R - q=}")